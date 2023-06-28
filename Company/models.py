59c1364afb0a8cc7fc844c72a4280f9562641419
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-06-28 18:50:07 +0100

    Created contact form model

--- Company/models.py
+++ Company/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
+class ContactForm(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    location = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    message = RichTextField()
+
+    def __str__(self):
+        return f"{self.full_name}"
+
+    class Meta:
+        verbose_name_plural = "Contact Forms"
