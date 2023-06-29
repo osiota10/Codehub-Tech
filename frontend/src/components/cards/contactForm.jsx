801f62ff3140cabab6f99b2e9b36e47bff78e854
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-06-29 17:28:59 +0100

    Setup email functionality to backend

--- frontend/src/components/cards/contactForm.jsx
+++ frontend/src/components/cards/contactForm.jsx
@@ -1,6 +1,57 @@
-import React from 'react'
+import { useState } from "react";
+import axios from 'axios';
 
-function ContactForm() {
+
+const ContactForm = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [formError, setFormError] = useState([]);
+    const [formData, setFormData] = useState({
+        full_name: '',
+        subject: '',
+        email: '',
+        phone_number: '',
+        message: '',
+    });
+
+    const { full_name, subject, email, phone_number, message } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        setLoading(true)
+
+        // declare the data fetching function
+        const PostFormData = async () => {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            };
+
+            const body = JSON.stringify({ full_name, subject, email, phone_number, message });
+
+            try {
+                const res = await axios.post(`${process.env.REACT_APP_API_URL}/contact-us`, body, config);
+                setLoading(false)
+                if (res.status === 201) {
+                    // handleShow()
+                    setFormData({
+                        full_name: '',
+                        subject: '',
+                        email: '',
+                        phone_number: '',
+                        message: '',
+                    })
+                }
+            } catch (err) {
+                setFormError(err.response.data);
+            }
+        }
+        PostFormData()
+    };
     return (
         <div className='container py-10'>
             <section className='row'>
@@ -10,43 +61,94 @@ function ContactForm() {
                         <h5>Tell us about it</h5>
                     </header>
 
-                    <form class="row g-3">
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="full_name" placeholder="name@example.com" />
+                    <form className="row g-3" onSubmit={e => onSubmit(e)}>
+                        <div className="col-md-6">
+                            <div className="form-floating">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="full_name"
+                                    placeholder="Full Name"
+                                    name="full_name"
+                                    onChange={e => onChange(e)}
+                                    value={full_name}
+                                    required
+                                />
+                                {/* <input type="text" className="form-control" id="full_name" placeholder="name@example.com" /> */}
                                 <label for="full_name">Full Name</label>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="phone_number" placeholder="name@example.com" />
+                        <div className="col-md-6">
+                            <div className="form-floating">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="phone_number"
+                                    placeholder="Full Name"
+                                    name="phone_number"
+                                    onChange={e => onChange(e)}
+                                    value={phone_number}
+                                    required
+                                />
+                                {/* <input type="text" className="form-control" id="phone_number" placeholder="name@example.com" /> */}
                                 <label for="phone_number">Phone Number</label>
                             </div>
                         </div>
 
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+                        <div className="col-md-6">
+                            <div className="form-floating">
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="email"
+                                    placeholder="Full Name"
+                                    name="email"
+                                    onChange={e => onChange(e)}
+                                    value={email}
+                                    required
+                                />
+                                {/* <input type="email" className="form-control" id="email" placeholder="name@example.com" /> */}
                                 <label for="email">Email address</label>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="subject" placeholder="name@example.com" />
+                        <div className="col-md-6">
+                            <div className="form-floating">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="subject"
+                                    placeholder="Full Name"
+                                    name="subject"
+                                    onChange={e => onChange(e)}
+                                    value={subject}
+                                    required
+                                />
+                                {/* <input type="text" className="form-control" id="subject" placeholder="name@example.com" /> */}
                                 <label for="subject">Subject</label>
                             </div>
                         </div>
 
-                        <div class="col-md-12">
-                            <div class="form-floating" >
-                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: '9.375rem' }}></textarea>
+                        <div className="col-md-12">
+                            <div className="form-floating" >
+                                <textarea
+                                    class="form-control"
+                                    id="floatingTextarea2"
+                                    rows="4"
+                                    placeholder="Leave a comment here"
+                                    name="message"
+                                    onChange={e => onChange(e)}
+                                    value={message}
+                                    style={{ height: '9.375rem' }}
+                                    required
+                                ></textarea>
+                                {/* <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: '9.375rem' }}></textarea> */}
                                 <label for="floatingTextarea2">Brief Description</label>
                             </div>
                         </div>
 
 
-                        <div class="col-12 d-grid">
-                            <button type="submit" class="btn btn-primary ">Submit</button>
+                        <div className="col-12 d-grid">
+                            <button type="submit" className="btn btn-primary ">Submit</button>
                         </div>
                     </form>
                 </section>
