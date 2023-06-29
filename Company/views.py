c0c7ccad7c80ea4e7d7b15ab7f853a2bd8322c1e
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-06-29 16:51:20 +0100

    Setup views for Contact form and email subscription

--- Company/views.py
+++ Company/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializer import *
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class ContactFormView(generics.CreateAPIView):
+    queryset = ContactForm.objects.all()
+    serializer_class = ContactFormSerializer
+    permission_classes = [AllowAny,]
+
+class EmailSubcriptionView(generics.CreateAPIView):
+    queryset = EmailSubcription.objects.all()
+    serializer_class = EmailSubcriptionSerializer
+    permission_classes = [AllowAny,]
