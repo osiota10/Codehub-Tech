8dd97402fd23098e017b8ddb155f9ddadb6c0009
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-06-30 10:03:50 +0100

    Work process frontend setup

--- frontend/src/components/cards/workProcess.jsx
+++ frontend/src/components/cards/workProcess.jsx
@@ -1,36 +1,30 @@
+import parse from 'html-react-parser';
 
 
-const pic = {
-    url: 'https://picsum.photos/200'
-}
-
-function WorkProcess() {
+const WorkProcess = ({ id, title, description, step, font_awesome_class }) => {
     return (
-        <section>
-            <section className="card custom-card" style={{ marginBottom: '1px' }}>
-                <div className="row g-0">
-                    <section className="card-body d-flex">
-                        <div className="col-md-2 d-flex flex-column align-items-center me-2">
-                            <i class="fa-sharp fa-solid fa-credit-card mb-1 fs-1 text-primary"></i>
-                            {/* <img src={pic.url} className="img-fluid mb-1" alt="..." style={{ maxWidth: '50px' }} /> */}
-                            <div className="bg-primary" style={{ minHeight: '50%' }}>
-                                <div className="vr" style={{ width: '4px' }}></div>
-                            </div>
+        <section className="card custom-card" style={{ marginBottom: '1px' }} key={id}>
+            <div className="row g-0">
+                <section className="card-body d-flex">
+                    <div className="col-md-2 d-flex flex-column align-items-center me-2">
+                        <i className={`${font_awesome_class} mb-1 fs-1 text-primary`}></i>
+                        <div className="bg-primary" style={{ minHeight: '50%' }}>
+                            <div className="vr" style={{ width: '4px' }}></div>
                         </div>
+                    </div>
 
-                        <section className="col-md-10">
-                            <header className="d-flex justify-content-between">
-                                <h5 className="card-title">Card title</h5>
-                                <div>
-                                    <span className="badge bg-primary align-middle">Step 1</span>
-                                </div>
-                            </header>
+                    <section className="col-md-10">
+                        <header className="d-flex justify-content-between">
+                            <h5 className="card-title">{title}</h5>
+                            <div>
+                                <span className="badge bg-primary align-middle">Step {step}</span>
+                            </div>
+                        </header>
 
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </section>
+                        <p className="card-text">{parse(`${description}`)}</p>
                     </section>
-                </div>
-            </section>
+                </section>
+            </div>
         </section>
     );
 }
--- frontend/src/components/groups/workProcessGroup.jsx
+++ frontend/src/components/groups/workProcessGroup.jsx
@@ -1,7 +1,20 @@
 import WorkProcess from "../cards/workProcess";
 import ImageHolder from "../cards/imageHolder";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
-function WorkProcessGroup() {
+
+const WorkProcessGroup = () => {
+    const [workProcess, setWorkProcess] = useState([])
+
+    useEffect(() => {
+        // Get Work Process Data
+        axios.get(`${process.env.REACT_APP_API_URL}/work-process`)
+            .then(res => {
+                setWorkProcess(res.data)
+            })
+    }, [])
+
     return (
         <section className="container py-10">
             <h2 className="text-center mb-8">Our Work Process</h2>
@@ -9,11 +22,17 @@ function WorkProcessGroup() {
                 <section className="col-lg-6">
                     <ImageHolder />
                 </section>
+
                 <section className="col-lg-6">
-                    <WorkProcess />
-                    <WorkProcess />
-                    <WorkProcess />
-                    <WorkProcess />
+                    {workProcess.map((item) =>
+                        <WorkProcess
+                            id={item.id}
+                            title={item.title}
+                            step={item.step}
+                            font_awesome_class={item.font_awesome_class}
+                            description={item.description}
+                        />
+                    )}
                 </section>
             </section>
         </section>
