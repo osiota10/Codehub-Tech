8b11a57803e2500ef60f7551970775ecd12dd479
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-01 17:48:39 +0100

    Testimonial frontend setup

--- frontend/src/components/cards/testimonials.jsx
+++ frontend/src/components/cards/testimonials.jsx
@@ -1,30 +1,24 @@
+import parse from 'html-react-parser';
 
-
-const pic = {
-    url: 'https://picsum.photos/200'
-}
-
-function TestimonialCard(image, key) {
+function TestimonialCard({ image, key, message, name, position }) {
     return (
         <>
             <div className='container '>
                 <div className='row' key={key}>
                     <div className='col-lg-5 d-flex justify-content-around align-items-center'>
                         <img src='Frame.svg' className="img-fluid" alt="..." style={{ maxWidth: '85.76px', maxHeight: '276px' }} />
-                        <img src={pic.url} className="img-fluid rounded-circle flex-grow-1" alt="..." style={{ maxWidth: '276px', maxHeight: '276px' }} />
+                        <img src={image} className="img-fluid rounded-circle flex-grow-1" alt="..." style={{ maxWidth: '276px', maxHeight: '276px' }} />
                         <img src='Frame.svg' className="img-fluid" alt="..." style={{ maxWidth: '85.76px', maxHeight: '276px' }} />
                     </div>
                     <div className='col-lg-7'>
                         <i class="fa-solid fa-quote-left text-primary fs-3"></i>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, asperiores.
-                            Officia impedit numquam, omnis veritatis dolor temporibus? Ea harum non soluta,
-                            porro obcaecati impedit cupiditate, illo labore nobis ipsa eaque.
+                        <p>{parse(`${message}`)}
                         </p>
                         <i class="fa-solid fa-quote-right text-primary d-flex justify-content-end fs-3"></i>
 
                         <div>
-                            <h5>Jane Cooper</h5>
-                            <small className='text-primary fw-bold'>CEO, ABC Company Ltd</small>
+                            <h5>{name}</h5>
+                            <small className='text-primary fw-bold'>{position}</small>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/groups/testimonialGroup.jsx
+++ frontend/src/components/groups/testimonialGroup.jsx
@@ -1,23 +1,45 @@
 import TestimonialCard from '../cards/testimonials';
-import { useContext } from 'react';
-import { TestimonialContext } from '../../App';
+import { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import { getSliderSettings } from './recentJobGroup';
+import axios from 'axios';
 
 function TeastimonialGroup() {
-    const test = useContext(TestimonialContext)
-    const items = Object.values(test);
+
+    const [testimonials, setTestimonials] = useState([])
 
     const sliderSettings = getSliderSettings(false, true, 1, true)
 
+    useEffect(() => {
+        // Testimonials
+        axios.get(`${process.env.REACT_APP_API_URL}/testimonials`)
+            .then(res => {
+                setTestimonials(res.data)
+            })
+    }, [])
     return (
-        <div className='container py-10'>
-            <h2 className='text-center mb-8'>Testimonials</h2>
+        <>
+            {
+                Object.keys(testimonials).length === 0
+                    ?
+                    null :
+                    <div className='container py-10'>
+                        <h2 className='text-center mb-8'>Testimonials</h2>
 
-            <Slider {...sliderSettings}>
-                {items.map(item => <TestimonialCard image={item.avatar} key={item.id} />)}
-            </Slider>
-        </div>
+                        <Slider {...sliderSettings}>
+                            {testimonials.map(item =>
+                                <TestimonialCard
+                                    image={item.get_image_url}
+                                    key={item.id}
+                                    name={item.name}
+                                    position={item.position}
+                                    message={item.message}
+                                />
+                            )}
+                        </Slider>
+                    </div>
+            }
+        </>
     );
 }
 
