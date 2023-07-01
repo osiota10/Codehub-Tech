bc409d317fc3234e5c43a0d4607a401642115b8c
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-01 15:55:42 +0100

    Hero section content setup

--- frontend/src/components/cards/hero.jsx
+++ frontend/src/components/cards/hero.jsx
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import { useContext } from 'react'
 import Slider from 'react-slick';
 import Stat from './stat';
+import { ServiceContext } from '../../App';
+import parse from 'html-react-parser';
+import { Link } from 'react-router-dom';
 
 const pic = {
     url: 'https://picsum.photos/300/200'
@@ -21,79 +24,62 @@ const settings = {
     },
 };
 
-class HeroSection extends Component {
+const HeroSection = () => {
+    const services = useContext(ServiceContext)
 
+    const myStyle = {
+        backgroundImage: `url(${pic.url})`,
+        height: '100%',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        boxShadow: 'inset 0 0 0 100vh rgba(235, 237, 240, 0.93)',
+    };
+    return (
+        <section className="card hero" >
+            <div className="">
+                <section className="ratio ratio-16x9" style={myStyle}>
 
-    state = {}
-    render() {
-        const myStyle = {
-            backgroundImage: `url(${pic.url})`,
-            height: '100%',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            boxShadow: 'inset 0 0 0 100vh rgba(235, 237, 240, 0.93)',
-        };
-        return (
-            <section className="card hero" >
-                <div className="">
-                    <section className="ratio ratio-16x9" style={myStyle}>
+                </section>
 
-                    </section>
+                <div className="card-img-overlay img-dark-overlay">
+                    <div className="container py-5 position-absolute top-50 start-50 translate-middle w-100">
 
-                    <div className="card-img-overlay img-dark-overlay">
-                        <div className="container py-5 position-absolute top-50 start-50 translate-middle w-100">
-                            <Slider {...settings}>
-                                <section className="custom-slider">
+                        <Slider {...settings}>
+                            {services.map((item) =>
+                                <section className="custom-slider" key={item.id}>
                                     <div className="row align-items-center">
                                         <div className="col-lg-6">
-                                            <h2>Web Development</h2>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-                                                purus sit amet luctus venenatis Lorem ipsum dolor sit amet, consectetur
-                                                adipiscing elit ut aliquam, purus sit amet purus sit amet luctus venenatis
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
-                                            </p>
-                                            <button className='btn btn-primary'>Request a Quote</button>
+                                            <h2>{item.title}</h2>
+                                            <p>{parse(`${item.description}`)}</p>
+                                            <Link
+                                                to={'/services/' + item.slug}
+                                                className='btn btn-primary text-decoration-none'
+                                            >
+                                                See More
+                                            </Link>
 
-                                            <section className="mt-4">
-                                                {/* <Stat /> */}
-                                            </section>
+                                            {
+                                                Object.keys(item.stats).length === 0
+                                                    ?
+                                                    null :
+                                                    <section className="mt-4">
+                                                        <Stat statList={item.stats} />
+                                                    </section>
+                                            }
                                         </div>
                                         <div className="col-lg-6 d-none d-lg-block">
                                             <img src="HeroImage.png" className='img-fluid ms-auto' alt="" />
                                         </div>
                                     </div>
                                 </section>
+                            )}
+                        </Slider>
 
-
-                                <section className="custom-slider">
-                                    <div className="row align-items-center">
-                                        <div className="col-lg-6">
-                                            <h2>Web Development</h2>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-                                                purus sit amet luctus venenatis Lorem ipsum dolor sit amet, consectetur
-                                                adipiscing elit ut aliquam, purus sit amet purus sit amet luctus venenatis
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
-                                            </p>
-                                            <button className='btn btn-primary'>Request a Quote</button>
-
-                                            <section className="mt-4">
-                                                {/* <Stat /> */}
-                                            </section>
-                                        </div>
-                                        <div className="col-lg-6 d-none d-lg-block">
-                                            <img src="HeroImage.png" className='img-fluid ms-auto ' alt="" />
-                                        </div>
-                                    </div>
-                                </section>
-                            </Slider>
-
-                        </div>
                     </div>
                 </div>
-            </section>
-
-        );
-    }
+            </div>
+        </section>
+    );
 }
 
 export default HeroSection;
