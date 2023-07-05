d1cf3287692c859508a48d11bfc6a7a82633e624
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-05 03:58:24 +0100

    Pic url

--- frontend/src/components/groups/workProcessGroup.jsx
+++ frontend/src/components/groups/workProcessGroup.jsx
@@ -3,6 +3,9 @@ import ImageHolder from "../cards/imageHolder";
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
+const pic = {
+    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.roberthalf.com%2Fsites%2Fdefault%2Ffiles%2F2020-01%2F01-music-at-work-rh.jpg&f=1&nofb=1&ipt=f1950c39be4f82691bf490135aff47961ee790f9f931301968cd065f553de62e&ipo=images'
+}
 
 const WorkProcessGroup = () => {
     const [workProcess, setWorkProcess] = useState([])
@@ -20,7 +23,7 @@ const WorkProcessGroup = () => {
             <h2 className="text-center mb-8">Our Work Process</h2>
             <section className="row g-4 align-items-center">
                 <section className="col-lg-6">
-                    <ImageHolder />
+                    <ImageHolder image={pic.url} />
                 </section>
 
                 <section className="col-lg-6">
