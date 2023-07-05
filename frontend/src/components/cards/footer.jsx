6ac4d4b79cee48d1e5fe53de0f22fe87c332fcc7
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-05 03:50:44 +0100

    Footer adjustments

--- frontend/src/components/cards/footer.jsx
+++ frontend/src/components/cards/footer.jsx
@@ -72,8 +72,8 @@ const Footer = () => {
                 </div>
             </div>
             <div style={{ borderTop: '1px solid #fff' }}>
-                <div className=' py-4'>
-                    <div className="row row-cols-1 row-cols-md-4 g-3 justify-content-center text-center">
+                <div className='container-fluid py-4'>
+                    <div className="row row-cols-1 row-cols-lg-3 g-3 justify-content-center text-center">
                         {
                             dataCheck
                                 ?
