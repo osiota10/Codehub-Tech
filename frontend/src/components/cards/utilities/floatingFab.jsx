138878e2936853728530f49d43cda51998227c18
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-13 15:40:44 +0100

    Added url

--- frontend/src/components/cards/utilities/floatingFab.jsx
+++ frontend/src/components/cards/utilities/floatingFab.jsx
@@ -1,12 +1,24 @@
-
+import { useContext } from "react";
+import { CompanyInfoContext } from "../../../App";
 
 const FabIcon = () => {
+    const companyInfo = useContext(CompanyInfoContext)
+    const whatsappUrlCheck = !companyInfo.company_social || companyInfo.company_social.length === 0 || !companyInfo.company_social.whatsapp_url
+
     return (
-        <a href="#" target="_blank" style={{ position: 'fixed', bottom: 48, right: 36 }}>
-            <span className="pulse-background">
-                <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
-            </span>
-        </a>
+        <>
+            {
+                whatsappUrlCheck
+                    ?
+                    null
+                    :
+                    <a href={companyInfo.company_social.whatsapp_url} target="_blank" style={{ position: 'fixed', bottom: 48, right: 36 }}>
+                        <span className="pulse-background">
+                            <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
+                        </span>
+                    </a>
+            }
+        </>
     );
 }
 
