e0a05696a51091b076ad97b171d74944e463f346
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-13 16:28:48 +0100

    Company info

--- frontend/src/components/cards/navBar.jsx
+++ frontend/src/components/cards/navBar.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Outlet, Link, NavLink } from "react-router-dom";
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { CompanyInfoContext } from "../../App";
 
 function NavBar() {
+    const companyInfo = useContext(CompanyInfoContext)
     // Sticky Navbar
     const [fix, seFix] = useState(false)
 
@@ -27,7 +29,14 @@ function NavBar() {
                 <div className="container-fluid">
                     <Link to="/" className='navbar-brand d-flex align-items-center'>
                         <img src="logo.svg" alt="Logo" width="50" height="50" className="d-inline-block align-text-top me-1" />
-                        <h6>Codehub <br />Technologies</h6>
+                        <h6>
+                            {companyInfo.company_name.split(' ').map((word, index) => (
+                                < span key={index}>
+                                    <span>{word}</span>
+                                    <br />
+                                </ span>
+                            ))}
+                        </h6>
                     </Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#offcanvasNavbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={handleOffcanvasShow}>
@@ -36,7 +45,7 @@ function NavBar() {
 
                     <Offcanvas id="offcanvasNavbar" show={showOffcanvas} onHide={handleOffcanvasClose} backdrop="static" responsive="lg" tabIndex="-1">
                         <Offcanvas.Header closeButton>
-                            <Offcanvas.Title>Codehub Technologies</Offcanvas.Title>
+                            <Offcanvas.Title>{companyInfo.company_name}</Offcanvas.Title>
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                             <div className="navbar-collapse justify-content-center custom-bar" id="navbarNav">
