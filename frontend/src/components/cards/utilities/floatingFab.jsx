7521b60447a159e71599a7d280d30517a1493ee9
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-16 11:26:50 +0100

    Achor tag rel attribute

--- frontend/src/components/cards/utilities/floatingFab.jsx
+++ frontend/src/components/cards/utilities/floatingFab.jsx
@@ -12,7 +12,7 @@ const FabIcon = () => {
                     ?
                     null
                     :
-                    <a href={companyInfo.company_social.whatsapp_url} target="_blank" style={{ position: 'fixed', bottom: 48, right: 36 }}>
+                    <a href={companyInfo.company_social.whatsapp_url} target="_blank" rel="noopener noreferrer" style={{ position: 'fixed', bottom: 48, right: 36 }}>
                         <span className="pulse-background">
                             <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
                         </span>
