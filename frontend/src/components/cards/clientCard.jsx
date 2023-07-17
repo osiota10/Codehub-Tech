6ce40dff3a58d77b8337f8a9de95ab8952a77545
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-17 17:48:37 +0100

    Justify contents

--- frontend/src/components/cards/clientCard.jsx
+++ frontend/src/components/cards/clientCard.jsx
@@ -3,7 +3,7 @@ import TextTruncate from 'react-text-truncate';
 function Client({ id, name, logo }) {
     return (
         <section className='card mx-auto' style={{ maxWidth: '379px' }} key={id}>
-            <div className='card-body d-flex align-items-center'>
+            <div className='card-body d-flex align-items-center justify-content-center'>
                 <img src={logo} className="card-img me-3" alt={`logo of ${name}`} style={{ maxWidth: '80px' }} />
 
                 <TextTruncate
