1a2f4ae9df704b611d7e28adb9a15216d8953720
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-10-23 15:13:26 +0100

    Image url

--- frontend/src/components/groups/workProcessGroup.jsx
+++ frontend/src/components/groups/workProcessGroup.jsx
@@ -1,57 +1,54 @@
 import WorkProcess from "../cards/workProcess";
 import ImageHolder from "../cards/imageHolder";
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const pic = {
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.roberthalf.com%2Fsites%2Fdefault%2Ffiles%2F2020-01%2F01-music-at-work-rh.jpg&f=1&nofb=1&ipt=f1950c39be4f82691bf490135aff47961ee790f9f931301968cd065f553de62e&ipo=images'
-}
+    url: "https://img.freepik.com/premium-photo/young-african-businesswoman-presenting-data-analysis-dashboard-tv-concord_31965-115410.jpg?w=900",
+};
 
 const WorkProcessGroup = () => {
-    const [workProcess, setWorkProcess] = useState([])
+    const [workProcess, setWorkProcess] = useState([]);
 
     useEffect(() => {
         // Get Work Process Data
-        axios.get(`${process.env.REACT_APP_API_URL}/work-process`)
-            .then(res => {
-                setWorkProcess(res.data)
-            })
-    }, [])
+        axios
+            .get(`${process.env.REACT_APP_API_URL}/work-process`)
+            .then((res) => {
+                setWorkProcess(res.data);
+            });
+    }, []);
 
     return (
         <>
-            {
-                Object.keys(workProcess).length === 0
-                    ?
-                    null
-                    :
-                    <section className="container py-10">
-                        <header className="text-center mb-4">
-                            <h2>Our Work Process</h2>
-                            <h6>The Magic Behind Our Work Process!</h6>
-                        </header>
+            {Object.keys(workProcess).length === 0 ? null : (
+                <section className="container py-10">
+                    <header className="text-center mb-4">
+                        <h2>Our Work Process</h2>
+                        <h6>The Magic Behind Our Work Process!</h6>
+                    </header>
 
-                        <section className="row g-4 align-items-center">
-                            <section className="col-lg-6">
-                                <ImageHolder image={pic.url} />
-                            </section>
+                    <section className="row g-4 align-items-center">
+                        <section className="col-lg-6">
+                            <ImageHolder image={pic.url} />
+                        </section>
 
-                            <section className="col-lg-6">
-                                {workProcess.map((item) =>
-                                    <WorkProcess
-                                        id={item.id}
-                                        title={item.title}
-                                        step={item.step}
-                                        font_awesome_class={item.font_awesome_class}
-                                        description={item.description}
-                                    />
-                                )}
-                            </section>
+                        <section className="col-lg-6">
+                            {workProcess.map((item) => (
+                                <WorkProcess
+                                    id={item.id}
+                                    title={item.title}
+                                    step={item.step}
+                                    font_awesome_class={item.font_awesome_class}
+                                    description={item.description}
+                                />
+                            ))}
                         </section>
                     </section>
-            }
+                </section>
+            )}
         </>
     );
-}
+};
 
 export default WorkProcessGroup;
