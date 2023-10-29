06fad92ac77f54244e46ec8141aabe9fea6da94a
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-10-29 17:27:06 +0100

    Bug fix

--- frontend/src/components/cards/team.jsx
+++ frontend/src/components/cards/team.jsx
@@ -1,26 +1,31 @@
 import Socials from "./socials";
 
-
 function OurTeam({ name, position, image, socialList }) {
-
     return (
-        <section className='col'>
-            <section className="card mx-auto" style={{ maxWidth: '379px' }} >
-
-                <div className='bg-primary athlete-bg' style={{ height: '120px' }} >
-                </div>
+        <section className="col">
+            <section className="card mx-auto" style={{ maxWidth: "379px" }}>
+                <div
+                    className="bg-primary athlete-bg"
+                    style={{ height: "120px" }}
+                ></div>
 
                 <section className="card-body fw-bolder text-primary">
                     <section className="d-flex justify-content-center mt-n12">
-                        <img src={image} className="mb-4 rounded-circle athlete-pix" width="170" height="170" alt="..." />
+                        <img
+                            src={image}
+                            className="mb-4 rounded-circle athlete-pix"
+                            width="170"
+                            height="170"
+                            alt={name}
+                        />
                     </section>
                     <section className="text-center">
                         <h5>{name}</h5>
                         <p>{position}</p>
-                        <Socials
-                            small={true}
-                            socials={socialList}
-                        />
+
+                        {socialList ? (
+                            <Socials small={true} socials={socialList} />
+                        ) : null}
                     </section>
                 </section>
             </section>
