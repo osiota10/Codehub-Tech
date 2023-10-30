b980fb7184f812d98ed0189a2f5017c2ac8a2bb7
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-10-30 18:52:52 +0100

    Whitenoise settings

--- backend/settings.py
+++ backend/settings.py
@@ -103,6 +103,7 @@ DJOSER = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
