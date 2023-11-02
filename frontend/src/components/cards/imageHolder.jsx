5c3bcd0f63c34eb93bf1a0a921fc43e522a8ba42
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-02 19:05:52 +0100

    Image placeholder

--- frontend/src/components/cards/imageHolder.jsx
+++ frontend/src/components/cards/imageHolder.jsx
@@ -1,10 +1,18 @@
-
-
 function ImageHolder({ image }) {
     return (
-        <div className='position-relative image-holder'>
-            <img src={process.env.PUBLIC_URL + '/Vector.svg'} className="img-fluid  image-holder-bg" alt="..." />
-            <img src={image} className="position-absolute top-50 start-50 translate-middle img-fluid" alt="..." style={{ maxWidth: '90%' }} />
+        <div className="position-relative image-holder">
+            <img
+                src={process.env.PUBLIC_URL + "/Vector.svg"}
+                className="img-fluid mx-auto d-block image-holder-bg"
+                alt="..."
+                style={{ maxWidth: "85%" }}
+            />
+            <img
+                src={image}
+                className="position-absolute top-50 start-50 translate-middle img-fluid rounded-4"
+                alt="..."
+                style={{ maxWidth: "90%" }}
+            />
         </div>
     );
 }
