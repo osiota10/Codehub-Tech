95e5ce38ea41b27356ffcd5fc5c949e0413289e2
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-11 17:01:11 +0100

    Font awesome size

--- frontend/src/components/cards/workProcess.jsx
+++ frontend/src/components/cards/workProcess.jsx
@@ -1,15 +1,23 @@
-import parse from 'html-react-parser';
-
+import parse from "html-react-parser";
 
 const WorkProcess = ({ id, title, description, step, font_awesome_class }) => {
     return (
-        <section className="card custom-card" style={{ marginBottom: '1px' }} key={id}>
+        <section
+            className="card custom-card"
+            style={{ marginBottom: "1px" }}
+            key={id}
+        >
             <div className="row g-0">
                 <section className="card-body d-flex">
                     <div className="col-md-2 d-flex flex-column align-items-center me-2">
-                        <i className={`${font_awesome_class} mb-1 fs-1 text-primary`}></i>
-                        <div className="bg-primary" style={{ minHeight: '50%' }}>
-                            <div className="vr" style={{ width: '4px' }}></div>
+                        <i
+                            className={`${font_awesome_class} mb-1 fs-3 text-primary`}
+                        ></i>
+                        <div
+                            className="bg-primary"
+                            style={{ minHeight: "50%" }}
+                        >
+                            <div className="vr" style={{ width: "4px" }}></div>
                         </div>
                     </div>
 
@@ -17,7 +25,9 @@ const WorkProcess = ({ id, title, description, step, font_awesome_class }) => {
                         <header className="d-flex justify-content-between">
                             <h5 className="card-title">{title}</h5>
                             <div>
-                                <span className="badge bg-primary align-middle">Step {step}</span>
+                                <span className="badge bg-primary align-middle">
+                                    Step {step}
+                                </span>
                             </div>
                         </header>
 
@@ -27,6 +37,6 @@ const WorkProcess = ({ id, title, description, step, font_awesome_class }) => {
             </div>
         </section>
     );
-}
+};
 
 export default WorkProcess;
