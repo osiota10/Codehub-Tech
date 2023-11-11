5b45effd199e40ad883f5cb0388947480dd1e81c
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-11 18:03:37 +0100

    Stat UI

--- frontend/src/components/cards/stat.jsx
+++ frontend/src/components/cards/stat.jsx
@@ -1,15 +1,12 @@
-
-
 function Stat({ statList }) {
-
     return (
         <section className="row row-cols-1 row-cols-lg-3 g-1 text-center justify-content-center">
-            {statList.map((item) =>
+            {statList.map((item) => (
                 <section className="col-4" key={item.id}>
-                    <h4>{item.stat_figure}+</h4>
-                    <small className="fw-light">{item.stat_title}</small>
+                    <h3>{item.stat_figure}+</h3>
+                    <p className="fw-bold text-primary">{item.stat_title}</p>
                 </section>
-            )}
+            ))}
         </section>
     );
 }
