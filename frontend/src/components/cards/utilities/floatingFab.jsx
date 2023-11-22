e9c4838517fac41883bfa1fdec811808c58b40be
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-22 15:27:42 +0100

    Pulse effect

--- frontend/src/components/cards/utilities/floatingFab.jsx
+++ frontend/src/components/cards/utilities/floatingFab.jsx
@@ -2,24 +2,28 @@ import { useContext } from "react";
 import { CompanyInfoContext } from "../../../App";
 
 const FabIcon = () => {
-    const companyInfo = useContext(CompanyInfoContext)
-    const whatsappUrlCheck = !companyInfo.company_social || companyInfo.company_social.length === 0 || !companyInfo.company_social.whatsapp_url
+    const companyInfo = useContext(CompanyInfoContext);
+    const whatsappUrlCheck =
+        !companyInfo.company_social ||
+        companyInfo.company_social.length === 0 ||
+        !companyInfo.company_social.whatsapp_url;
 
     return (
         <>
-            {
-                whatsappUrlCheck
-                    ?
-                    null
-                    :
-                    <a href={companyInfo.company_social.whatsapp_url} target="_blank" rel="noopener noreferrer" style={{ position: 'fixed', bottom: 48, right: 36 }}>
-                        <span className="pulse-background">
-                            <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
-                        </span>
-                    </a>
-            }
+            {whatsappUrlCheck ? null : (
+                <a
+                    href={companyInfo.company_social.whatsapp_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ position: "fixed", bottom: 28, right: 28 }}
+                >
+                    <span className="pulse-background">
+                        <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
+                    </span>
+                </a>
+            )}
         </>
     );
-}
+};
 
 export default FabIcon;
