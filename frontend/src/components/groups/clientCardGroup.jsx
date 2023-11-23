adf68a4f731b958d41654e932276f9964f2a9948
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-23 09:36:13 +0100

    Client context usage

--- frontend/src/components/groups/clientCardGroup.jsx
+++ frontend/src/components/groups/clientCardGroup.jsx
@@ -1,47 +1,36 @@
-import { useEffect, useState } from 'react';
-import Client from '../cards/clientCard';
+import { useEffect, useState, useContext } from "react";
+import Client from "../cards/clientCard";
 import Slider from "react-slick";
-import { getSliderSettings } from './recentJobGroup';
-import axios from 'axios';
+import { getSliderSettings } from "./recentJobGroup";
+import { ClientContext } from "../../App";
 
 const ClientGroup = () => {
-  const settings = getSliderSettings(false, false, 3, false)
+    const settings = getSliderSettings(false, false, 3, false);
 
-  const [client, setClient] = useState([])
+    const client = useContext(ClientContext);
 
-  useEffect(() => {
-    // Clients
-    axios.get(`${process.env.REACT_APP_API_URL}/clients`)
-      .then(res => {
-        setClient(res.data)
-      })
-  }, [])
+    return (
+        <>
+            {Object.keys(client).length === 0 ? null : (
+                <div className="container py-10">
+                    <header className="text-center mb-3">
+                        <h2>Our Clients</h2>
+                        <h6>Trusted by these Remarkable Clients</h6>
+                    </header>
 
-  return (
-    <>
-      {Object.keys(client).length === 0
-        ?
-        null
-        :
-        <div className='container py-10'>
-          <header className='text-center mb-3'>
-            <h2>Our Clients</h2>
-            <h6>Trusted by these Remarkable Clients</h6>
-          </header>
-
-          <Slider {...settings}>
-            {client.map(item =>
-              <Client
-                id={item.id}
-                name={item.name_of_client}
-                logo={item.get_logo_url}
-              />
+                    <Slider {...settings}>
+                        {client.map((item) => (
+                            <Client
+                                id={item.id}
+                                name={item.name_of_client}
+                                logo={item.get_logo_url}
+                            />
+                        ))}
+                    </Slider>
+                </div>
             )}
-          </Slider>
-        </div>
-      }
-    </>
-  );
-}
+        </>
+    );
+};
 
 export default ClientGroup;
