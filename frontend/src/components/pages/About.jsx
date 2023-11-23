c797681abb4f5316e5c2e196a300e1d1dac093d4
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-23 09:43:39 +0100

    Core value context usage

--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,64 +1,50 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from "react";
 import ClientGroup from "../groups/clientCardGroup";
-import IndustryGroup from '../groups/industryGroup';
-import PageTitle from './pageTitle';
-import ContactForm from '../cards/contactForm';
-import MailSubscription from '../cards/emailSub';
-import TextTemplate from '../cards/sampleText';
-import Technologies from '../groups/techGroup';
-import ServiceGroup from '../groups/serviceCardGroup';
-import axios from 'axios';
-
+import IndustryGroup from "../groups/industryGroup";
+import PageTitle from "./pageTitle";
+import ContactForm from "../cards/contactForm";
+import MailSubscription from "../cards/emailSub";
+import TextTemplate from "../cards/sampleText";
+import Technologies from "../groups/techGroup";
+import ServiceGroup from "../groups/serviceCardGroup";
+import axios from "axios";
+import { CoreValuesContext } from "../../App";
 
 const About = () => {
-  const [tech, setTech] = useState([])
-  const [coreValue, setCorevalue] = useState([])
+    const [tech, setTech] = useState([]);
+    const coreValue = useContext(CoreValuesContext);
 
-  useEffect(() => {
-    //Our Technologies
-    axios.get(`${process.env.REACT_APP_API_URL}/technologies`)
-      .then(res => {
-        setTech(res.data)
-      })
+    useEffect(() => {
+        //Our Technologies
+        axios
+            .get(`${process.env.REACT_APP_API_URL}/technologies`)
+            .then((res) => {
+                setTech(res.data);
+            });
+    }, []);
 
-    //Our Core Values
-    axios.get(`${process.env.REACT_APP_API_URL}/core-values`)
-      .then(res => {
-        setCorevalue(res.data)
-      })
-  }, [])
+    return (
+        <>
+            <PageTitle title="About" />
 
-  return (
-    <>
-      <PageTitle title="About" />
+            {/* Core Value list */}
+            {Object.keys(coreValue).length === 0 ? null : (
+                <TextTemplate dataList={coreValue} />
+            )}
 
-      {/* Core Value list */}
-      {
-        Object.keys(coreValue).length === 0
-          ?
-          null
-          :
-          <TextTemplate dataList={coreValue} />
-      }
+            <ServiceGroup type={false} />
 
+            {/* Technologies list */}
+            {Object.keys(tech).length === 0 ? null : (
+                <Technologies techList={tech} />
+            )}
 
-      <ServiceGroup type={false} />
-
-      {/* Technologies list */}
-      {
-        Object.keys(tech).length === 0
-          ?
-          null
-          :
-          <Technologies techList={tech} />
-      }
-
-      <IndustryGroup />
-      <ClientGroup />
-      <MailSubscription />
-      <ContactForm />
-    </>
-  );
-}
+            <IndustryGroup />
+            <ClientGroup />
+            <MailSubscription />
+            <ContactForm />
+        </>
+    );
+};
 
 export default About;
