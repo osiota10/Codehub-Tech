0451b764891898aa0f96fff9b2a35fa852012f48
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-24 17:44:45 +0100

    FAB icon size

--- frontend/src/components/cards/utilities/floatingFab.jsx
+++ frontend/src/components/cards/utilities/floatingFab.jsx
@@ -15,10 +15,10 @@ const FabIcon = () => {
                     href={companyInfo.company_social.whatsapp_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    style={{ position: "fixed", bottom: 28, right: 28 }}
+                    style={{ position: "fixed", bottom: 28, right: 16 }}
                 >
                     <span className="pulse-background">
-                        <i className="fa-brands fa-whatsapp fs-3 position-absolute top-50 start-50 translate-middle text-white"></i>
+                        <i className="fa-brands fa-whatsapp fs-4 position-absolute top-50 start-50 translate-middle text-white"></i>
                     </span>
                 </a>
             )}
