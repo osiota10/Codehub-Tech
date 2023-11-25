5a38d3277c3010bf9354cd651d85d22e3906d07b
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-25 10:34:24 +0100

    FAB z-index

--- frontend/src/components/cards/utilities/floatingFab.jsx
+++ frontend/src/components/cards/utilities/floatingFab.jsx
@@ -15,7 +15,12 @@ const FabIcon = () => {
                     href={companyInfo.company_social.whatsapp_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    style={{ position: "fixed", bottom: 28, right: 16 }}
+                    style={{
+                        position: "fixed",
+                        bottom: 28,
+                        right: 16,
+                        zIndex: "9991",
+                    }}
                 >
                     <span className="pulse-background">
                         <i className="fa-brands fa-whatsapp fs-4 position-absolute top-50 start-50 translate-middle text-white"></i>
