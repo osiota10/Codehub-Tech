53d9caf5b69964e539874eb62c1db706456110de
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-11-25 17:55:35 +0100

    404 page UI

--- frontend/src/components/pages/NoPage.jsx
+++ frontend/src/components/pages/NoPage.jsx
@@ -1,29 +1,54 @@
-
+import { Link } from "react-router-dom";
 
 const pic = {
-  url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.roberthalf.com%2Fsites%2Fdefault%2Ffiles%2F2020-01%2F01-music-at-work-rh.jpg&f=1&nofb=1&ipt=f1950c39be4f82691bf490135aff47961ee790f9f931301968cd065f553de62e&ipo=images'
-}
-
-const getStyle = (imgUrl) => {
-  return {
-    backgroundImage: `linear-gradient(to right, rgb(0, 237, 240), rgba(255, 255, 255, 0.0)), url(${imgUrl})`,
-    height: '100%',
-    width: '100%',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    // boxShadow: 'inset 0 0 0 100vh rgba(235, 237, 240, 0.93)',
-  };
+    url: "https://img.freepik.com/premium-photo/abstract-plexus-geometrical-shapes-connection-concept-digital-communication-technology-network-background_34629-858.jpg?w=996",
 };
 
+const getStyle = (imgUrl) => {
+    return {
+        backgroundImage: `url(${imgUrl})`,
+        height: "100vh",
+        width: "100%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        // boxShadow: 'inset 0 0 0 100vh rgba(235, 237, 240, 0.93)',
+    };
+};
 
 const NoPage = () => {
-  return (
-    <section>
-      <section className="ratio ratio-21x9" style={getStyle(pic.url)}>
-        <h1>404</h1>
-      </section>
-    </section>
-  );
+    return (
+        <section
+            style={getStyle(pic.url)}
+            className="d-flex justify-content-center align-items-center text-center"
+        >
+            <section className="container">
+                <section className="row text-white justify-content-center">
+                    <section className="col-lg-6">
+                        <i
+                            style={{ fontSize: "6rem" }}
+                            class="fa-solid fa-triangle-exclamation"
+                        ></i>
+                        <h1 style={{ fontSize: "5rem" }} className="text-white">
+                            Oops!
+                        </h1>
+                        <h4 className="text-white">
+                            404 Error - Page not Found
+                        </h4>
+                        <p>
+                            The page you are looking for might have been removed
+                            or had its name changed or is temporary unavailable
+                        </p>
+                        <Link
+                            className="btn btn btn-light text-decoration-none"
+                            to="/"
+                        >
+                            Go to Homepage
+                        </Link>
+                    </section>
+                </section>
+            </section>
+        </section>
+    );
 };
 
 export default NoPage;
