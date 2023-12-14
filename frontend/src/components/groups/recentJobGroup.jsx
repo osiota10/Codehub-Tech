2a0ce7edf7b12781438232a3827cf1fd92a99d0b
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-12-14 21:27:53 +0100

    Slider settings

--- frontend/src/components/groups/recentJobGroup.jsx
+++ frontend/src/components/groups/recentJobGroup.jsx
@@ -1,119 +1,133 @@
-import React, { useContext } from 'react';
-import RecentJobCard from '../cards/recentJobCard';
+import React, { useContext } from "react";
+import RecentJobCard from "../cards/recentJobCard";
 import Slider from "react-slick";
-import { JobContext } from '../../App';
-import { Link } from 'react-router-dom';
+import { JobContext } from "../../App";
+import { Link } from "react-router-dom";
 
-export const getSliderSettings = (dots, arrows, slidesToShow, removeAutoPlay) => {
-  const sliderSettings = {
-    dots: dots,
-    arrows: arrows,
-    className: "center",
-    infinite: false,
-    centerPadding: "160px",
-    slidesToShow: slidesToShow,
-    swipeToSlide: true,
-    autoplay: true,
-    speed: 5000,
-    autoplaySpeed: 5000,
-    cssEase: "linear",
-    afterChange: (index) => {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    },
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-    ]
-  };
+export const getSliderSettings = ({
+    dots,
+    arrows,
+    slidesToShow,
+    removeAutoPlay,
+    infinite,
+}) => {
+    const sliderSettings = {
+        dots: dots,
+        arrows: arrows,
+        className: "center",
+        infinite: infinite,
+        centerPadding: "160px",
+        slidesToShow: slidesToShow,
+        swipeToSlide: true,
+        autoplay: true,
+        // speed: 5000,
+        // autoplaySpeed: 5000,
+        cssEase: "linear",
+        afterChange: (index) => {
+            console.log(
+                `Slider Changed to: ${
+                    index + 1
+                }, background: #222; color: #bada55`
+            );
+        },
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                },
+            },
+        ],
+    };
 
-  // Conditionally hide some properties
-  if (removeAutoPlay) {
-    delete sliderSettings.autoplay;
-    delete sliderSettings.speed;
-    delete sliderSettings.autoplaySpeed;
-  }
+    // Conditionally hide some properties
+    if (removeAutoPlay) {
+        delete sliderSettings.autoplay;
+        delete sliderSettings.speed;
+        delete sliderSettings.autoplaySpeed;
+    }
 
-  return sliderSettings;
+    return sliderSettings;
 };
 
-
 function RecentJobGroup({ filterCategory, currentItem }) {
-  const sliderSettings = getSliderSettings(true, true, 3, true)
+    const sliderSettings = getSliderSettings(true, true, 3, true);
 
-  const jobs = useContext(JobContext)
+    const jobs = useContext(JobContext);
 
-  // Extract category names from the passed category object
-  const categoryNames = filterCategory ? filterCategory.map(cat => cat.name) : [];
+    // Extract category names from the passed category object
+    const categoryNames = filterCategory
+        ? filterCategory.map((cat) => cat.name)
+        : [];
 
-  const filteredItems = categoryNames.length
-    ? jobs.filter(item => {
-      // Assuming item.category is a list of objects
-      return (
-        item.category.some(cat => categoryNames.includes(cat.name)) &&
-        item.id !== currentItem
-      );
-    })
-    : jobs;
+    const filteredItems = categoryNames.length
+        ? jobs.filter((item) => {
+              // Assuming item.category is a list of objects
+              return (
+                  item.category.some((cat) =>
+                      categoryNames.includes(cat.name)
+                  ) && item.id !== currentItem
+              );
+          })
+        : jobs;
 
-  return (
-    <>
-      {
-        Object.keys(filteredItems).length === 0
-          ?
-          null
-          :
-          <div className='container py-10'>
-            <header className='text-center mb-3'>
-              <h2 >
-                {filterCategory ? 'Related Jobs Done' : 'Our Recent Jobs'}
-              </h2>
-              <h6>
-                {filterCategory ? 'Explore Related Jobs!' : 'Explore Our Recent Jobs!'}
-              </h6>
-            </header>
+    return (
+        <>
+            {Object.keys(filteredItems).length === 0 ? null : (
+                <div className="container py-10">
+                    <header className="text-center mb-3">
+                        <h2>
+                            {filterCategory
+                                ? "Related Jobs Done"
+                                : "Our Recent Jobs"}
+                        </h2>
+                        <h6>
+                            {filterCategory
+                                ? "Explore Related Jobs!"
+                                : "Explore Our Recent Jobs!"}
+                        </h6>
+                    </header>
 
-
-            <Slider {...sliderSettings}>
-              {filteredItems.slice(0, 9).map(item =>
-                <Link
-                  className='text-decoration-none'
-                  to={'/recent-jobs/' + item.slug}
-                >
-                  <RecentJobCard
-                    id={item.id}
-                    title={item.title}
-                    body={item.safe_summary_html}
-                    category={item.category}
-                    thumbnail={item.get_image_url}
-                    date={item.date}
-                  />
-                </Link>
-              )}
-            </Slider>
-            <div className='d-flex justify-content-center'>
-              <Link to="/recent-jobs" className='btn btn-primary mt-6'>See all Jobs</Link>
-            </div>
-          </div>
-      }
-    </>
-  );
+                    <Slider {...sliderSettings}>
+                        {filteredItems.slice(0, 9).map((item) => (
+                            <Link
+                                className="text-decoration-none"
+                                to={"/recent-jobs/" + item.slug}
+                            >
+                                <RecentJobCard
+                                    id={item.id}
+                                    title={item.title}
+                                    body={item.safe_summary_html}
+                                    category={item.category}
+                                    thumbnail={item.get_image_url}
+                                    date={item.date}
+                                />
+                            </Link>
+                        ))}
+                    </Slider>
+                    <div className="d-flex justify-content-center">
+                        <Link
+                            to="/recent-jobs"
+                            className="btn btn-primary mt-6"
+                        >
+                            See all Jobs
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default RecentJobGroup;
--- frontend/src/components/groups/serviceCardGroup.jsx
+++ frontend/src/components/groups/serviceCardGroup.jsx
@@ -1,86 +1,93 @@
-import React from 'react';
-import Service from '../cards/serviceCard';
-import { useContext } from 'react';
-import { ServiceContext } from '../../App';
+import React from "react";
+import Service from "../cards/serviceCard";
+import { useContext } from "react";
+import { ServiceContext } from "../../App";
 import { Link } from "react-router-dom";
-import Slider from 'react-slick';
-import { getSliderSettings } from './recentJobGroup';
-
+import Slider from "react-slick";
+import { getSliderSettings } from "./recentJobGroup";
 
 function ServiceGroup({ type }) {
-  const service = useContext(ServiceContext)
+    const service = useContext(ServiceContext);
 
-  const settings = getSliderSettings(false, true, 3, false)
+    const settings = getSliderSettings({
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+    });
 
-  return (
-    <>
-      {
-        Object.keys(service).length === 0
-          ?
-          null
-          :
-          <div className='container py-10'>
-            {type ?
-              <>
-                <header className='text-center mb-4'>
-                  <h2>Our Services</h2>
-                  <h6>Discover Our Exceptional Services</h6>
-                </header>
+    return (
+        <>
+            {Object.keys(service).length === 0 ? null : (
+                <div className="container py-10">
+                    {type ? (
+                        <>
+                            <header className="text-center mb-4">
+                                <h2>Our Services</h2>
+                                <h6>Discover Our Exceptional Services</h6>
+                            </header>
 
-                <div className='row row-cols-1 row-cols-lg-3 g-6 justify-content-center'>
-                  {service.slice(0, 6).map(item =>
-                    <Link className='text-decoration-none'
-                      to={'/services/' + item.slug}
-                    >
-                      <Service
-                        key={item.id}
-                        title={item.title}
-                        body={item.safe_description_html}
-                        image={item.get_image_url}
-                      />
-                    </Link>
-                  )}
+                            <div className="row row-cols-1 row-cols-lg-3 g-6 justify-content-center">
+                                {service.slice(0, 6).map((item) => (
+                                    <Link
+                                        className="text-decoration-none"
+                                        to={"/services/" + item.slug}
+                                    >
+                                        <Service
+                                            key={item.id}
+                                            title={item.title}
+                                            body={item.safe_description_html}
+                                            image={item.get_image_url}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
+                            {Object.keys(service).length > 6 ? (
+                                <div className="d-flex justify-content-center">
+                                    <Link
+                                        to="/services"
+                                        className="btn btn-primary mt-6"
+                                    >
+                                        See all Services
+                                    </Link>
+                                </div>
+                            ) : null}
+                        </>
+                    ) : (
+                        <div className="container py-10">
+                            <header className="text-center mb-3">
+                                <h2>What We Do</h2>
+                                <h6>Discover Our Exceptional Services</h6>
+                            </header>
+
+                            <Slider {...settings}>
+                                {service.map((item) => (
+                                    <Link
+                                        className="text-decoration-none"
+                                        to={"/services/" + item.slug}
+                                    >
+                                        <Service
+                                            key={item.id}
+                                            title={item.title}
+                                            body={item.safe_description_html}
+                                            image={item.get_image_url}
+                                        />
+                                    </Link>
+                                ))}
+                            </Slider>
+                            <div className="d-flex justify-content-center">
+                                <Link
+                                    to="/services"
+                                    className="btn btn-primary mt-6"
+                                >
+                                    See all Services
+                                </Link>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {Object.keys(service).length > 6 ?
-                  <div className='d-flex justify-content-center'>
-                    <Link to="/services" className='btn btn-primary mt-6'>See all Services</Link>
-                  </div>
-                  :
-                  null}
-              </>
-
-              :
-
-              <div className='container py-10'>
-                <header className='text-center mb-3'>
-                  <h2>What We Do</h2>
-                  <h6>Discover Our Exceptional Services</h6>
-                </header>
-
-                <Slider {...settings}>
-                  {service.map(item =>
-                    <Link
-                      className='text-decoration-none'
-                      to={'/services/' + item.slug}
-                    >
-                      <Service
-                        key={item.id}
-                        title={item.title}
-                        body={item.safe_description_html}
-                        image={item.get_image_url}
-                      />
-                    </Link>
-                  )}
-                </Slider>
-                <div className='d-flex justify-content-center'>
-                  <Link to="/services" className='btn btn-primary mt-6'>See all Services</Link>
-                </div>
-              </div>
-            }
-          </div>
-      }
-    </>
-  );
+            )}
+        </>
+    );
 }
 
 export default ServiceGroup;
