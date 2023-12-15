150f22c1ea6d1c2cb1cd1d34df90b9b078372e3d
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-12-15 12:54:26 +0100

    Google map bug

--- frontend/src/components/cards/googleMap.jsx
+++ frontend/src/components/cards/googleMap.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const GoogleMap = () => {
     useEffect(() => {
         // Load the Google Maps API script dynamically
-        const script = document.createElement('script');
+        const script = document.createElement("script");
         script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_API_URL}&libraries=places`;
         script.async = true;
         script.defer = true;
-        document.body.appendChild(script);
 
-        // Initialize the map once the API script is loaded
-        script.onload = initMap;
+        // Callback function to initialize the map
+        script.onload = () => {
+            initMap();
+        };
+
+        // Handle error in case the script fails to load
+        script.onerror = () => {
+            console.error("Error loading Google Maps API script.");
+        };
+
+        document.body.appendChild(script);
 
         // Clean up the script tag on component unmount
         return () => {
@@ -20,16 +28,16 @@ const GoogleMap = () => {
 
     const initMap = () => {
         // Create a new map instance
-        const map = new window.google.maps.Map(document.getElementById('map'), {
-            center: { lat: 6.207449, lng: 6.722695 }, // Set the initial map center coordinates
-            zoom: 17, // Set the initial zoom level
+        const map = new window.google.maps.Map(document.getElementById("map"), {
+            center: { lat: 6.207449, lng: 6.722695 },
+            zoom: 17,
         });
 
         // Add a marker at the business location
         new window.google.maps.Marker({
             position: { lat: 6.207449, lng: 6.722695 },
             map: map,
-            title: 'Back Shops, Stephen Keshi Stadium, Asaba, Nigeria',
+            title: "Back Shops, Stephen Keshi Stadium, Asaba, Nigeria",
         });
 
         // Add any additional map configurations or markers as needed
@@ -37,7 +45,7 @@ const GoogleMap = () => {
         // Example: new window.google.maps.Marker({ position: { lat: 37.7749, lng: -122.4194 }, map });
     };
 
-    return <div id="map" style={{ height: '400px' }}></div>;
+    return <div id="map" style={{ height: "400px" }}></div>;
 };
 
 export default GoogleMap;
