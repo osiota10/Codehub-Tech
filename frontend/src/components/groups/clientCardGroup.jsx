f4deb43a19bf934e1a64528615d92f7e3296d4eb
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-01-16 19:08:39 +0100

    Client Card redesign

--- frontend/src/components/groups/clientCardGroup.jsx
+++ frontend/src/components/groups/clientCardGroup.jsx
@@ -12,22 +12,44 @@ const ClientGroup = () => {
     return (
         <>
             {Object.keys(client).length === 0 ? null : (
-                <div className="container py-10">
-                    <header className="text-center mb-3">
-                        <h2>Our Clients</h2>
-                        <h6>Trusted by these Remarkable Clients</h6>
-                    </header>
+                <section className="py-10">
+                    <section className="container">
+                        <header className="text-center mb-3">
+                            <h2>Our Clients</h2>
+                            <h6>Trusted by these Remarkable Clients</h6>
+                        </header>
 
-                    <Slider {...settings}>
-                        {client.map((item) => (
-                            <Client
-                                id={item.id}
-                                name={item.name_of_client}
-                                logo={item.get_logo_url}
-                            />
-                        ))}
-                    </Slider>
-                </div>
+                        <Slider {...settings}>
+                            {client.map((item) => (
+                                <>
+                                    {/* <Client
+                                        id={item.id}
+                                        name={item.name_of_client}
+                                        logo={item.get_logo_url}
+                                    /> */}
+                                    <section
+                                        className="mx-auto tint"
+                                        style={{
+                                            maxWidth: "279px",
+                                            filter: "grayscale(100%)",
+                                        }}
+                                        id={item.id}
+                                    >
+                                        <div className="card-body d-flex flex-row align-items-center justify-content-center">
+                                            <img
+                                                src={item.get_logo_url}
+                                                className="card-img me-2"
+                                                alt={`logo of`}
+                                                style={{ maxWidth: "80px" }}
+                                            />
+                                            <h6>{item.name_of_client}</h6>
+                                        </div>
+                                    </section>
+                                </>
+                            ))}
+                        </Slider>
+                    </section>
+                </section>
             )}
         </>
     );
