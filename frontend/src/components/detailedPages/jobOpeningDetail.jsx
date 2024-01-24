577244541a8021656d98a07db45b7a8bb6628673
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-01-24 08:31:49 +0100

    Page UI

---
+++ frontend/src/components/detailedPages/jobOpeningDetail.jsx
@@ -0,0 +1,79 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import PageTitle from "../pages/pageTitle";
+import axios from "axios";
+import ContactForm from "../cards/contactForm";
+import MailSubscription from "../cards/emailSub";
+import parse from "html-react-parser";
+import { CompanyInfoContext } from "../../App";
+import { PageLoader } from "../cards/utilities/loader";
+
+const JobOpeningDetail = () => {
+    const { slug } = useParams();
+    const [detail, setDetails] = useState([]);
+
+    // const dataCheck = !detail || detail.length === 0;
+    // const companyInfo = useContext(CompanyInfoContext);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/our-jobs/` + slug
+                );
+                setDetails(response.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                // Set loading to false once the API call is complete (whether it succeeded or failed)
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [slug]);
+
+    return (
+        <>
+            <PageTitle title={detail.title} />
+            <section className="container">
+                <section className="row">
+                    <section className="col-lg-8 mx-auto">
+                        <h2 className="text-center">{detail.title}</h2>
+
+                        <h6>Job Summary</h6>
+                        <ul>
+                            <li>Date Posted: {detail.date_created}</li>
+                            <li>Deadline: {detail.deadline}</li>
+                            <li>
+                                Number of Openings: {detail.number_of_openings}
+                            </li>
+                            <li>
+                                Years of Experience:
+                                {detail.years_of_experience}
+                            </li>
+                            <li>Locaion: {detail.location}</li>
+                            <li>Skills: {detail.skills}</li>
+                        </ul>
+                    </section>
+                </section>
+
+                <section className="row">
+                    <section className="col-lg-8 mx-auto">
+                        <h2 className="text-center">
+                            Roles and Responsibilites
+                        </h2>
+                        {parse(`${detail.roles}`)}
+                    </section>
+                </section>
+            </section>
+
+            <MailSubscription />
+            <ContactForm />
+            {loading ? <PageLoader /> : null}
+        </>
+    );
+};
+
+export default JobOpeningDetail;
