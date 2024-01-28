b1bfadbc2dc16c6e31df39ded9030b11b4412e09
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-01-28 03:12:00 +0100

    Content Development

--- frontend/src/components/groups/careerGroup.jsx
+++ frontend/src/components/groups/careerGroup.jsx
@@ -22,14 +22,14 @@ const CareerGroup = () => {
         <>
             {Object.keys(jobOpenings).length === 0 ? (
                 <section className="container text-center">
-                    <h2>There are currently no availiable Positions</h2>
-                    <h6>Check back later for available positions</h6>
+                    <h2>We are currently NOT Recruiting</h2>
+                    <h6>Stay tuned for future opportunities!</h6>
                 </section>
             ) : (
                 <section className="container">
                     <header className="text-center mb-3">
                         <h2>Availiable Positions</h2>
-                        <h6>Checkout the available positions below</h6>
+                        <h6>Discover our current job openings below</h6>
                     </header>
 
                     <Slider {...settings}>
